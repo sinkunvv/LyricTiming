8a208437a46ee056b35259c4edd6c2dae9284105
sinkunvv <sinkunvv.dev@gmail.com>
2019-10-31 18:04:13 +0900

    キーボード式に変更

--- Scripts/LyricTiming.cs
+++ Scripts/LyricTiming.cs
@@ -6,144 +6,62 @@ using UnityEditor;
 public class LyricTiming : MonoBehaviour
 {
     public bool ViewMode;
-    public Animator animator;
     public AnimationClip animClip;
     public AudioSource audioSource;
     public float audioTime;
-    public GameObject lyricParent;
-    public GameObject[] lyrics;
+    public GameObject[] Targets;
 
-    GameObject[] ExistLyrics;
+    Animator animator;
+    Transform Lyric;
+    bool[] isTargets;
     Dictionary<string, List<Keyframe>> keys;
 
+    GUIStyle style;
+    GUIStyleState styleState;
 
     void Start()
     {
-        EditorApplication.playModeStateChanged += OnChangedPlayMode;
-        foreach (var lyric in lyrics)
+        if (!ViewMode)
         {
-            keys.Add(lyric.name, new List<Keyframe>());
-            keys[lyric.name].Add(new Keyframe(0.0f, 0.0f, float.PositiveInfinity, float.NegativeInfinity));
-            //keys[lyric.name].Add(new Keyframe(0.1f, 0.0f));
+            EditorApplication.playModeStateChanged += OnChangedPlayMode;
+            keys = new Dictionary<string, List<Keyframe>>();
+            foreach (var lyric in Targets)
+            {
+                keys.Add(lyric.name, new List<Keyframe>());
+                keys[lyric.name].Add(new Keyframe(0.0f, 0.0f, float.PositiveInfinity, float.NegativeInfinity));
+                lyric.gameObject.SetActive(false);
+            }
+
+            // 存在するオブジェクトは対象外とする
+            ExistsProparty();
+
+            // GUIスタイル
+            style = new GUIStyle();
+            styleState = new GUIStyleState();
+            styleState.textColor = Color.red;
+            style.normal = styleState;
         }
     }
 
     // Inspector変更時初期化
     void OnValidate()
     {
-        audioSource.time = audioTime;
+        Lyric = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
 
         var info = animator.GetCurrentAnimatorStateInfo(0);
+        audioSource.time = audioTime;
         animator.Play(info.fullPathHash, 0, audioTime / info.length);
         animator.enabled = true;
-
-
-        keys = new Dictionary<string, List<Keyframe>>();
-        if (lyricParent)
-        {
-            var child = lyricParent.transform.childCount;
-            lyrics = new GameObject[child];
-            ExistLyrics = new GameObject[child];
-            var cnt = 0;
-            foreach (Transform lyric in lyricParent.transform)
-            {
-                lyrics[cnt] = lyric.gameObject;
-                lyric.gameObject.SetActive(false);
-                cnt++;
-            }
-
-            // 既存アニメーションチェック
-            ExistsCurve();
-        }
     }
 
-    void OnGUI()
-    {
-        if (lyrics.Length > 0 && !ViewMode)
-        {
-            var x = 0;
-            var y = 0;
-            for (var i = 0; i < lyrics.Length; i++)
-            {
-                // 既存のアニメーションが存在しないオブジェクトだけボタン配置
-                if (lyrics[i] != ExistLyrics[i])
-                {
-                    if (GUI.Button(new Rect(130 * x, 40 * y, 120, 30), (!lyrics[i].activeSelf ? "Enable:" : "Disable:") + lyrics[i].name))
-                    {
-                        // Start:0 Stop:1
-                        // Start KeyFrame Constant
-                        keys[lyrics[i].name].Add(new Keyframe(audioTime, !lyrics[i].activeSelf ? 1.0f : 0.0f, float.PositiveInfinity, float.NegativeInfinity));
-
-                        lyrics[i].SetActive(!lyrics[i].activeSelf);
-                    }
-                    x++;
-
-                    // ボタン改行
-                    if (x > 5)
-                    {
-                        x = 1;
-                        y++;
-                    }
-                }
-            }
-        }
-
-    }
-
-    // Update is called once per frame
+    // 
     void Update()
     {
         audioTime += Time.deltaTime;
 
-    }
+        DownKeyCheck();
 
-    void SaveAnimeClip()
-    {
-        // 元AnimationClipバックアップ
-        string path = AssetDatabase.GetAssetPath(animClip.GetInstanceID());
-        string backup = "-" + DateTime.Now.ToString("hhmmssfff") + ".anim";
-
-        AssetDatabase.CopyAsset(path, path.Replace(".anim", backup));
-        Debug.Log("AnimationClip退避: " + path.Replace(".anim", backup));
-
-        foreach (var lyric in lyrics)
-        {
-            // 最終フレーム DisableでKeyFrame追加
-            keys[lyric.name].Add(new Keyframe(audioSource.clip.length, 0.0f, float.PositiveInfinity, float.NegativeInfinity));
-
-            // Binging
-            EditorCurveBinding curveBinding = new EditorCurveBinding();
-            curveBinding.path = lyric.name;
-            curveBinding.type = typeof(GameObject);
-            curveBinding.propertyName = "m_IsActive";
-
-            // AnimationCurve 登録したKeyFramesで初期化
-            AnimationCurve curve = new AnimationCurve(keys[lyric.name].ToArray());
-
-            // 既存のAnimationCurve検索
-            if (animClip)
-            {
-                foreach (var binding in AnimationUtility.GetCurveBindings(animClip))
-                {
-                    // 既存のAnimationCurveが存在すれば上書き
-                    if (binding.path == lyric.name)
-                    {
-                        curve = AnimationUtility.GetEditorCurve(animClip, binding);
-
-                        // 登録したKeyFrameをAnimationCurveに追加
-                        foreach (var key in keys[lyric.name])
-                        {
-                            curve.AddKey(key);
-                        }
-                        break;
-                    }
-                }
-            }
-            // AnimationCurve保存
-            AnimationUtility.SetEditorCurve(animClip, curveBinding, curve);
-        }
-
-        Debug.Log("アニメーション保存..!");
     }
 
     void OnChangedPlayMode(PlayModeStateChange state)
@@ -154,19 +72,154 @@ public class LyricTiming : MonoBehaviour
         }
     }
 
-    void ExistsCurve()
+    // AnimationClip保存
+    void SaveAnimeClip()
     {
-        for (var i = 0; i < lyrics.Length; i++)
+        // 元AnimationClipバックアップ
+        string path = AssetDatabase.GetAssetPath(animClip.GetInstanceID());
+        string backup = "-" + DateTime.Now.ToString("hhmmssfff") + ".anim";
+
+        AssetDatabase.CopyAsset(path, path.Replace(".anim", backup));
+        Debug.Log("AnimationClip退避: " + path.Replace(".anim", backup));
+
+        for (var i = 0; i < Targets.Length; i++)
         {
+            // 最終フレーム DisableでKeyFrame追加
+            keys[Targets[i].name].Add(new Keyframe(audioSource.clip.length, 0.0f, float.PositiveInfinity, float.NegativeInfinity));
+
+            // Binging
+            EditorCurveBinding curveBinding = new EditorCurveBinding();
+            curveBinding.path = GethierarchyPath(Targets[i]);
+            curveBinding.type = typeof(GameObject);
+            curveBinding.propertyName = "m_IsActive";
+
+            // AnimationCurve 登録したKeyFramesで初期化
+            AnimationCurve curve = new AnimationCurve(keys[Targets[i].name].ToArray());
+
+            // AnimationCurve保存
+            AnimationUtility.SetEditorCurve(animClip, curveBinding, curve);
+        }
+
+        Debug.Log("アニメーション保存..!");
+    }
+
+    // 入力キー判定
+    void DownKeyCheck()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(code))
+                {
+                    switch (code)
+                    {
+                        case KeyCode.Alpha1:
+                            SetKeyFrame(0);
+                            break;
+                        case KeyCode.Alpha2:
+                            SetKeyFrame(1);
+                            break;
+                        case KeyCode.Alpha3:
+                            SetKeyFrame(2);
+                            break;
+                        case KeyCode.Alpha4:
+                            SetKeyFrame(3);
+                            break;
+                        case KeyCode.Alpha5:
+                            SetKeyFrame(4);
+                            break;
+                        case KeyCode.Alpha6:
+                            SetKeyFrame(5);
+                            break;
+                        case KeyCode.Alpha7:
+                            SetKeyFrame(6);
+                            break;
+                        case KeyCode.Alpha8:
+                            SetKeyFrame(7);
+                            break;
+                        case KeyCode.Alpha9:
+                            SetKeyFrame(8);
+                            break;
+                        case KeyCode.Alpha0:
+                            SetKeyFrame(9);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    void SetKeyFrame(int i)
+    {
+        Debug.Log(i);
+        // Start KeyFrame Constant
+        if (i < Targets.Length && !ViewMode)
+        {
+            keys[Targets[i].name].Add(new Keyframe(audioTime, !Targets[i].activeSelf ? 1.0f : 0.0f, float.PositiveInfinity, float.NegativeInfinity));
+            Targets[i].SetActive(!Targets[i].activeSelf);
+        }
+    }
+
+
+    // 親オブジェクトからのパスを取得
+    string GethierarchyPath(GameObject target)
+    {
+        Transform _target = target.transform.parent;
+        Transform _parent = Lyric;
+        string path = target.name;
+
+        while (_parent.name != _target.name)
+        { 
+            path = _target.name + "/" + path;
+            _target = _target.parent;
+        }
+
+        return path;
+    }
+
+    // AnimationPropartyの存在チェック
+    void ExistsProparty()
+    {
+        isTargets = new bool[Targets.Length];
+        for (var i = 0; i < Targets.Length; i++)
+        {
+            isTargets[i] = true;
             // 既存のAnimationCurve検索
             if (animClip)
             {
                 foreach (var binding in AnimationUtility.GetCurveBindings(animClip))
                 {
-                    if (binding.path == lyrics[i].name)
+                    if (binding.path == GethierarchyPath(Targets[i]))
                     {
-                        ExistLyrics[i] = lyrics[i];
+                        isTargets[i] = false;
+                        Targets[i].SetActive(true);
+                        break;
                     }
+                }
+            }
+
+        }
+    }
+
+    // Game画面に表示
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 0, 250, 30), audioSource.time.ToString() +"s");
+        
+
+        if (Targets.Length > 0 && !ViewMode)
+        {
+            GUI.Label(new Rect(10, 30, 500, 30), "【AnimationClipに書き込めないオブジェクトは赤色で表示しています】", style);
+            for (var i = 0; i < Targets.Length; i++)
+            {
+                if (isTargets[i])
+                {
+                    GUI.Label(new Rect(10, 30 * (i + 2), 250, 20), i + 1 + ":" + Targets[i].name);
+                }
+                else
+                {
+                    GUI.Label(new Rect(10, 30 * (i + 2), 250, 20), i + 1 + ":" + Targets[i].name, style);
                 }
             }
         }
